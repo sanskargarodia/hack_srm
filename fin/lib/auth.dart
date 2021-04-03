54aaefb526e5a8bedcffb6320b7abaee92c36210
sanskargarodia <45456555+sanskargarodia@users.noreply.github.com>
2021-04-03 13:13:33 +0530

    added Auth service

--- fin/lib/auth.dart
+++ fin/lib/auth.dart
@@ -1,13 +1,81 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fin/user.dart';
 
-class AuthService extends StatefulWidget {
-  @override
-  _AuthServiceState createState() => _AuthServiceState();
-}
+import 'globals.dart' as globals;
 
-class _AuthServiceState extends State<AuthService> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // create user object based on firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    if (user != null) {
+      globals.user_id = user.uid;
+    }
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  //sign in anamously
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      globals.user_id = user.uid;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      globals.user_id = user.uid;
+      globals.email = email;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //register with email and password
+  Future registerWithEmailAndPassword(
+      String name, String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      globals.user_id = user.uid;
+      globals.email = email;
+      Firestore.instance
+          .collection('user_details')
+          .add({'name': name, 'email': email, 'password': password});
+      //create a new document for the user with the uid
+      //await DatabaseService(uid: user.uid).updateUserData(name, 0);
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //signout
+  Future signout() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
+
